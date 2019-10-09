71f99eec7f61c5a9cabf992a1216a046ae8e604c
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-10-09 16:18:01 -0500

    Contact module Added

---
+++ src/js/modules/contact.js
@@ -0,0 +1,6 @@
+const contactContent = (() => {
+  const content = '<h2>You can\'t touch me, I\'m intangible, remember?</h2><h3>just kidding, you can check where I <a class="outside-link" href="https://github.com/codingAngarita/Restaurant-JS" target="_blank">currently live</a> or meet the <a href="https://github.com/codingAngarita" class="outside-link" target="_blank">chef</a></h3><h2>(⚆ _ ⚆)</h2>';
+  return content;
+});
+
+export default contactContent
