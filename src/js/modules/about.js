5e1caf195dec52506c1d889bf0e5e0376cb36726
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-10-09 16:17:52 -0500

    About module added

---
+++ src/js/modules/about.js
@@ -0,0 +1,6 @@
+const aboutContent = (() => {
+  const content = '<h2>We are a pioneers in the intangible restaurant market, serving the best functions in the whole web.</h2><h3>what about you?</h3><h3>(⚆ _ ⚆)</h3>';
+  return content;
+});
+
+export default aboutContent
