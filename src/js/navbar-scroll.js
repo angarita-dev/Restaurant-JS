9c85dc606e801e4ffb2bb2803d78c0f6a3c2d6a8
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-10-08 22:56:58 -0500

    navbar scroll logic

--- src/js/navbar-scroll.js
+++ src/js/navbar-scroll.js
@@ -3,8 +3,10 @@ window.onscroll = function() { scrollFunction() };
 function scrollFunction() {
 	x = document.getElementById("navbar");
   if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
-		x.classList.add('down');
+    x.classList.add('nav-down');
+    x.classList.remove('nav-up');
   } else {
-		x.classList.remove('down');
+    x.classList.remove('nav-down');
+    x.classList.add('nav-up');
   }
 }
