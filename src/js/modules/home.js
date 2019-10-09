0db7c717452df86fba7d0d163fa8bd07fe06fe7a
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-10-09 16:18:10 -0500

    Home module added

---
+++ src/js/modules/home.js
@@ -0,0 +1,6 @@
+const homeContent = (() => {
+  const content = '<h2>Welcome to our intangible restaurant</h2><h4>You can ask for the <span class="code">menu()</span> over there, in the <span class="code">developer console</span></h4>';
+  return content;
+});
+
+export default homeContent
