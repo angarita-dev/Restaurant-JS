ad69070c4f468a523062d7006f882b1b337c9f31
codingAngarita <44899916+codingAngarita@users.noreply.github.com>
2019-10-14 10:09:46 -0500

    Linting errors

--- src/js/modules/digital-menu.js
+++ src/js/modules/digital-menu.js
@@ -1,7 +1,7 @@
 const menu = (() => {
   const menu = () => {
     console.log('Welcome to the intangible restaurant!, this is today\'s menu: \n\t 1.RegExp soup. \n\t 2.Recursive salad. \n\t 3.Chef\'s specialty. \n\n When you make up your mind just order(plate)');
-  }
+  };
   const order = (plate) => {
     switch (plate) {
       case 1:
@@ -17,9 +17,9 @@ const menu = (() => {
         console.log('hmm, that plate is not available today, maybe tomorrow?');
         break;
     }
-  }
+  };
   window.menu = menu;
   window.order = order;
 })
 
-export default menu; 
+export default menu;
